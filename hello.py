f1cf55d246defd92e79056f727ffa49cb45bc8be
flavinhodogera <162146730+flavinhodogera@users.noreply.github.com>
2024-04-25 11:34:13 -0300

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class MinhaApp(App):
-    def build(self):
-        return Label(text='Olá Senai!')
-    
-if __name__ == "__main__":
-    MinhaApp().run()
