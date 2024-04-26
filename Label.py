91120a3ed84dd331ee9a530d3702c77297b73019
flavinhodogera <162146730+flavinhodogera@users.noreply.github.com>
2024-04-26 12:05:05 -0300

    Add files via upload

---
+++ Label.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class Myapp(App):
+    def build(self):
+        return Label(
+            text='Olá, SENAI!',
+            halign='left',   #Alinha o texto à esquerda
+            valign='top',    #Alinha o texto ao topo
+            size_hint=(None, None),  #Desativa o Ajuste automático do tamanho
+            size=(150, 50),  #Define o tamanho do rótulon
+            text_size=(150, None)  #Define a largura máxima do texto
+        )
+    
+
+if __name__ == '__main__':
+    Myapp().run()
